6a4dc220b62a55ab75ef910b095dd65e020c6ad7
Mariya Dorofeyeva <maria5dorofeeva@gmail.com>
2021-08-04 20:45:15 +0300

    подготовка к деплою, добавление данных

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,62 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
-module.exports = {
-    entry: __dirname + "/src/main.js",
-    module: {
-        rules: [
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                use: [
-                    'url-loader?limit=10000',
-                    'img-loader'
-                ]
-            },
-
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'fonts/'
+module.exports = (env) => {
+    return {
+        entry: __dirname + "/src/main.js",
+        module: {
+            rules: [
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "sass-loader",
+                    ],
+                },
+                {
+                    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: 'fonts/'
+                            }
                         }
-                    }
-                ]
-            },
-            { test: /\.(js)$/, use: 'babel-loader' },
-            {
-                test: /\.hbs/,
-                loader: 'handlebars-loader',
-                options: {
-                    helperDirs: [
-                        path.join(__dirname, "/src/helpers/"),
                     ]
                 },
-                exclude: /(node_modules|bower_components)/
-            },
+                { test: /\.(js)$/, use: 'babel-loader' },
+                {
+                    test: /\.hbs/,
+                    loader: 'handlebars-loader',
+                    options: {
+                        helperDirs: [
+                            path.join(__dirname, "/src/helpers/"),
+                        ]
+                    },
+                    exclude: /(node_modules|bower_components)/
+                },
 
 
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname + '/dist',),
-        filename: 'bundle.js',
-        publicPath: '/'
-    },
-    plugins: [
-        new CopyWebpackPlugin({ patterns: [{ from: 'src/data', to: 'api' }] }),
-        new HtmlWebpackPlugin({
-            template: __dirname + "/public/index.html",
-            inject: 'body'
-        }),
-        new MiniCssExtractPlugin()
+            ]
+        },
+        output: {
+            path: path.resolve(__dirname + '/dist',),
+            filename: 'bundle.js',
+            publicPath: env.production ? '/apartment_list' : '/'
+        },
+        plugins: [
+            new CopyWebpackPlugin({ patterns: [{ from: 'public/style/img', to: 'img' }] }),
+            new HtmlWebpackPlugin({
+                template: __dirname + "/public/index.html",
+                inject: 'body'
+            }),
+            new MiniCssExtractPlugin()
 
-    ],
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+        ],
+        mode: env.production  ? 'production' : 'development'
+    }
 }
